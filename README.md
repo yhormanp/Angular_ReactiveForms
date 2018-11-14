4c7c4549bfa63edf37f563672c9cf315669f0601
Yhorman Perez <jhppunk@gmail.com>
2018-11-14 10:25:37 -0500

    example finished until module 7, dynamically duplicate input elements

---
+++ README.md
@@ -0,0 +1,62 @@
+
+# Demo
+
+this project was created to understand how to work with :
+template-driven forms
+reactive-forms
+learn to use formGroup
+setValue and patchValue
+simplify with FormBuilder
+set validation rules
+create custom validators
+create custom validators with parameters
+validate several fiels with the same validator (email, confirm email)
+add or remove validator dynamically
+watch for changes in a control
+how to duplicate formgroups and addit to the formbuilder
+
+
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
+
+This project was taken as base from 
+
+angular2-ReactiveForms
+https://github.com/DeborahK/Angular-ReactiveForms
+
+try 'npm install' to download the packages
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+
+-------------------------------------------------------------------
+
+
+
+# Demo
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,148 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, AbstractControl, ValidatorFn, FormArray } from "@angular/forms";
+import { NgForm, Validators } from "@angular/forms";
+import  { debounceTime } from 'rxjs/operators'
+import { Customer } from "./customer";
+
+function emailMatcher (c: AbstractControl): { [key: string]: boolean } |  null {
+  const emailControl = c.get('email');
+  const confirmControl =c.get('confirmEmail');
+
+  if (emailControl.pristine ||  confirmControl.pristine)
+  {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value)
+  {
+    return null;
+  }
+
+  return { 'match': true};
+
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      c.value != undefined &&
+      (isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return { range: true };
+    }
+    return null;
+  };
+}
+
+@Component({
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"]
+})
+export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+  customer = new Customer();
+  emailMessage: string;
+  get addresses(): FormArray{
+    return <FormArray> this.customerForm.get('addresses');
+  }
+  
+  /**
+   *
+   */
+
+   private validationMessages = {
+     required: 'please enter your email address',
+     email: 'please enter a valid email address'
+   }
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      // lastName: {value :'n/a', disabled:true},
+      lastName: ["", [Validators.required, Validators.minLength(5)]],
+      emailGroup: this.fb.group({
+        email: [
+          "",
+          // [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]
+          [Validators.required, Validators.email]
+        ],
+        confirmEmail: ['', Validators.required],
+      }, { validator: emailMatcher} ),
+     
+      phone: "",
+      notification: "email",
+      rating: ["", ratingRange(1, 5)],
+      sendCatalog: true,
+      // addresses:this.buildAddress()
+      addresses:this.fb.array([this.buildAddress() ])
+     
+    });
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    )
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    )
+
+    // this.customerForm= new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true)
+    // });
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
+  buildAddress(): FormGroup {
+    return  this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    })
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: "Jack",
+      lastName: "Harkness1",
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log("Saved: " + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c:AbstractControl): void {
+    this.emailMessage ="";
+    if ((c.touched || c.dirty) && c.errors)
+    {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.emailMessage += this.validationMessages[key]).join(' ');
+     }
+  }
+
+  setNotification(notifiVia: string): void {
+    const phoneControl = this.customerForm.get("phone");
+    if (notifiVia === "text") {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+}
